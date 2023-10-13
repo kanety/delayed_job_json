0cd1c6eabb6bbd4b6cee16f1b1f95c5dec531a42
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2023-10-13 13:48:54 +0900

    Test on rails 7.1 and postgres 15

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -8,8 +8,9 @@ require 'delayed_job_json'
 
 module Dummy
   class Application < Rails::Application
+    config.load_defaults Rails::VERSION::STRING.to_f
+
     config.active_job.queue_adapter = :delayed_job
-    config.active_record.legacy_connection_handling = false if Rails.gem_version > Gem::Version.new('6.1')
 
     if ENV['DATABASE'].in?(%w(postgresql mysql))
       config.paths["config/database"] = "config/database_#{ENV['DATABASE']}.yml"
